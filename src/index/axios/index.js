c80f6bf21e4706084c64ab5439d9fbcf1c29ff3e
zz-fe <214464812@qq.com>
2018-03-31 17:03:20 +0800

    修改上传权限

--- src/index/axios/index.js
+++ src/index/axios/index.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
-import Qs from 'qs' 
-
-
-// const url = 'http://10.252.55.245:8112'
-
-const url = ''
+import Qs from 'qs'
+let url = ''
+process.env.NODE_ENV == 'development' ? url = 'http://10.252.55.245:8112' : ''
+     
 export const baseUrl = `${url}`
 //请求前
 axios.interceptors.request.use(
@@ -24,3 +22,4 @@ axios.interceptors.response.use(function (response) {
 }, function (error) {
     console.log(error,'error')
 });
+
